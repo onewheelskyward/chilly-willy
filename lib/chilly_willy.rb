585d8c082c4970416631d58d8173c184c6b947e2
Andrew Kreps <andrew.kreps@gmail.com>
2014-09-09 11:51:15 -0700

    Blind test writes.

--- lib/chilly_willy.rb
+++ lib/chilly_willy.rb
@@ -1,3 +1,31 @@
 class ChillyWilly
+  @key = nil
+  @cooldown = nil
+  @start_time = nil
+
+  def initialize(key, cooldown)
+    self.key = key
+    self.cooldown = cooldown
+    self.start_time = Time.now
+  end
+
+  def expired?
+    if Time.now - self.start_time >= self.cooldown
+      true
+    end
+  end
+
+  def time_to_expire
+    tte = self.cooldown - Time.now - self.start_time
+    if tte < 0
+      0
+    else
+      tte
+    end
+  end
+
+  def expire
+    self.cooldown = 0
+  end
 
 end
--- spec/chilly_willy_spec.rb
+++ spec/chilly_willy_spec.rb
@@ -3,8 +3,35 @@ require_relative '../lib/chilly_willy'
 
 describe 'ChillyWilly tests' do
   it 'exists' do
-    chilly = ChillyWilly.new
+    chilly = ChillyWilly.new('key', 1)
     expect(chilly).to_not be(nil)
     expect(chilly.is_a?(ChillyWilly)).to eq(true)
   end
+
+  it 'stores a cooldown' do
+    chilly = ChillyWilly.new('key', 60)
+    expect(chilly.timeleft).to eq(60)
+  end
+
+  it 'tests a cooldown expiration' do
+    chilly = ChillyWilly.new('key', 1)
+    sleep(1)
+    expect(chilly.expired?).to eq(true)
+  end
+
+  it 'tests a cooldown time until expiration' do
+    chilly = ChillyWilly.new('key', 10)
+    sleep(1)
+    expect(chilly.time_to_expire).to eq(9)
+  end
+
+  it 'expires immediately' do
+    chilly = ChillyWilly.new('key', 10000)
+    expect(chilly.time_to_expire).to eq(10000)
+    expect(chilly.expired?).to eq(false)
+    chilly.expire
+    expect(chilly.expired?).to eq(true)
+    expect(chilly.time_to_expire).to eq(0)
+  end
+
 end
